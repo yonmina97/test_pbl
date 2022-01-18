8f8c78d9739aeed889bb412b1045e19cc06012d8
Jin's coding <65287872+yonmina97@users.noreply.github.com>
2022-01-18 17:54:20 +0900

    Add files via upload

---
+++ realtest.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hell():
+    return "<h1>hello Flask~~!! hahaha<h1>"
+
+
+@app.route('/test')
+def test():
+    #수행해야 할 로직이 여기 들어온다.
+    return "test"
+
+if __name__== '__main__':
+    app.run("0.0.0.0",port=8080)
